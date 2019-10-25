21a1a2a5b5fee37a59aa70aaec350a3b55116659
bashard001 <bdaseh@gmail.com>
2019-10-24 20:15:19 -0700

    started weather api

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 var APIkey = "tOFft5fp4kxxA1Xq7PQ6ManIDhkIdcqZ";
-var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=US&apikey=" + APIkey;
+var city = "";
 
+
+$("button").on("click", function(){
+    var caContent = $("#get-weather")
+
+            var letsGo = caContent.val()
+          city = letsGo
+var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=sport&city=" + city + "&apikey=" + APIkey;
 
 $.ajax({
     url: queryURL,
     method: "GET"
 }).then(function(response) {
     console.log(response)
+    console.log(response._embedded.events[0].name)
+    $(".city").html("event name: " + response._embedded.events[0].name)
+
 });
+
+})
