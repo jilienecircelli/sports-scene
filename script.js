ef9c937acb3bc35bfbce54978561c6af8cf4a243
Jiliene Circelli <jilienecircelli@gmail.com>
2019-10-29 21:33:05 -0700

    updated index, css & script

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 var APIkey = "tOFft5fp4kxxA1Xq7PQ6ManIDhkIdcqZ";
 var city = "";
 
-
+function initMap(lat, lng) {
+    // The location of event
+    var latLng = { lat: lat, lng: lng };
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), { zoom: 11, center: latLng });
+    // The marker, positioned at event venue
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: map
+    });
+}
 
 $("button").on("click", function() {
     $(".search-area").removeClass();
@@ -19,54 +30,56 @@ $("button").on("click", function() {
     }).then(function(response) {
         console.log(response)
         console.log(response._embedded.events[0].name)
+        var event = response._embedded.events[0]
+        var venue = response._embedded.events[0]._embedded.venues[0]
 
         var eventCards = $(".event-data")
 
-        var card = $("<div>");
-        card.addClass("event-card");
-        eventCards.append(card);
+        var card = $("<div class='event-card row'>");
+
 
         // Event Name
 
         // Pull event date and reverse string
-        var eventDate = response._embedded.events[0].dates.start.localDate;
-        var date = eventDate.split("-").reverse().join("-")
-        card.append("<div class='event-title'>" + response._embedded.events[0].name + "<div class='date-info'>" + date)
+        var leftColumn = $("<div class='col s12 m6'></div>")
+        var rightColumn = $("<div class='col s12 m6'></div>")
+        card.append(leftColumn)
+        card.append(rightColumn)
 
-        // Event Date
-        // card.append("<div>" + date)
-        // Event Venue
-        card.append("<div>" + "Venue: " + response._embedded.events[0]._embedded.venues[0].name)
-            // Event Images
-        card.append("<div>" + "<img src=" + response._embedded.events[0]._embedded.attractions[0].images[0].url + ">" + "<img src=" + response._embedded.events[0]._embedded.attractions[1].images[0].url + ">")
-            // Link to purchase tickets
-        card.append("<a href=" + response._embedded.events[0].url + ">Click here to purchase tickets!</a>")
+        // Event Date shortening
+        var date = event.dates.start.localDate.split("-").reverse().join("-")
+            // Event Name
+        var eventNameEl = $("<h2 class='event-title'>" + event.name + "</h2>")
+            // Event Date
+        var eventDateEl = $("<div class='date-info'>" + date + "</div>")
+            // Event Venue
+        var venueNameEl = $("<div>" + "Venue: " + venue.name + "</div>");
+        // Event Images
+        var eventImageEl = $("<img src=" + event._embedded.attractions[0].images[0].url + ">" + "<img src=" + event._embedded.attractions[1].images[0].url + ">");
+        // Link to purchase tickets
+        var eventUrlEl = $("<a id='tix' href=" + event.url + ">Click here to purchase tickets!</a>");
+
+
+        leftColumn.append(eventNameEl)
+        leftColumn.append(eventDateEl)
+        leftColumn.append(venueNameEl)
+        leftColumn.append(eventImageEl)
+
+        rightColumn.append("<div id='map'>")
 
 
         // Setting variables for lat/lng of map
-        var latitude = response._embedded.events[0]._embedded.venues[0].location.latitude;
-        var longitude = response._embedded.events[0]._embedded.venues[0].location.longitude;
+        var latitude = venue.location.latitude;
+        var longitude = venue.location.longitude;
 
         var lat = parseFloat(latitude);
         var lng = parseFloat(longitude)
 
         // // Map Function
 
-        function initMap() {
-            // The location of event
-            var latLng = { lat: lat, lng: lng };
-            // The map, centered at Uluru
-            var map = new google.maps.Map(
-                document.getElementById('map'), { zoom: 4, center: latLng });
-            // The marker, positioned at event venue
-            var marker = new google.maps.Marker({
-                position: latLng,
-                map: map
-            });
-        }
 
-        // Append map below link
-        card.append("<div id='map'>")
-        initMap()
+
+        eventCards.append(card);
+        initMap(lat, lng)
     });
 })
