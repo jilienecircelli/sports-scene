2a686f0e5b023c3b232809cee0cada83f254be55
bashard001 <bdaseh@gmail.com>
2019-11-01 14:46:15 -0700

    fixed the the problem of the link to buy the ticket to have its own line in the column and added the time of the evets in the event cards

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function embedTheMap(index) {
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
+  }).then(function (response) {
     var venue = response._embedded.events[index]._embedded.venues[0];
     var latitude = venue.location.latitude;
     var longitude = venue.location.longitude;
@@ -60,7 +60,7 @@ function pullingEvents() {
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function(response) {
+  }).then(function (response) {
     console.log(response);
     console.log(response._embedded.events.length);
 
@@ -85,6 +85,35 @@ function pullingEvents() {
       rightColumn.append(`<div id='map${i}' class='map'>`);
       embedTheMap(i);
 
+      var time = event.dates.start.localTime
+
+      time = time.split(':'); // convert to array
+
+      // fetch
+      var hours = Number(time[0]);
+      var minutes = Number(time[1]);
+
+
+      // calculate
+      var timeValue;
+
+      if (hours > 0 && hours <= 12) {
+        timeValue = "" + hours;
+      } else if (hours > 12) {
+        timeValue = "" + (hours - 12);
+      } else if (hours == 0) {
+        timeValue = "12";
+      }
+
+      timeValue += (minutes < 10) ? ":0" + minutes : ":" + minutes;  // get minutes
+
+      timeValue += (hours >= 12) ? " P.M." : " A.M.";  // get AM/PM
+
+      // show
+
+      console.log(timeValue);
+
+
       // Event Date shortening
       var date = event.dates.start.localDate
         .split("-")
@@ -93,24 +122,24 @@ function pullingEvents() {
       // Event Name
       var eventNameEl = $("<h2 class='event-title'>" + event.name + "</h2>");
       // Event Date
-      var eventDateEl = $("<div class='date-info'>" + date + "</div>");
+      var eventDateEl = $("<div class='date-info'>" + date + " at: " + timeValue + "</div>");
       // Event Venue
       var venueNameEl = $("<div>" + "Venue: " + venue.name + "</div>");
       // Event Images
       var eventImageEl = $(
         "<img src=" +
-          event._embedded.attractions[0].images[0].url +
-          ">" +
-          "<img src=" +
-          event._embedded.attractions[1].images[0].url +
-          ">"
+        event._embedded.attractions[0].images[0].url +
+        ">" +
+        "<img src=" +
+        event._embedded.attractions[1].images[0].url +
+        ">"
       );
       // Link to purchase tickets
 
       var eventUrlEl = $(
-        "<a target='_blank' id='tix' href=" +
-          event.url +
-          ">Click here to purchase tickets!</a>"
+        "<a target='_blank' class='tixlink' id='tix' href=" +
+        event.url +
+        ">Click here to purchase tickets!</a>"
       );
 
       var genre = event.classifications[0].genre.name;
@@ -138,6 +167,7 @@ function pullingEvents() {
         leftColumn.append(sportsLogo);
       }
 
+      
       middleColumn.append(eventNameEl);
       middleColumn.append(eventDateEl);
       middleColumn.append(venueNameEl);
@@ -152,79 +182,79 @@ function pullingEvents() {
 
 function currentlocationWeather() {
 
-    var apiKey = "03e24d7d731fc83efc64f5aa4eb937c1";
-    navigator.geolocation.getCurrentPosition(function (position) {
-      longitude = position.coords.longitude;
-      latitude = position.coords.latitude;
-      console.log(longitude)
-      var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
-      $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-        .then(function (response) {
-          console.log(response)
-          var iconcode = response.weather[0].icon;
-          var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-  
-          // Log the queryURL
-          console.log(queryURL);
-  
-          // Log the resulting object
-          console.log(response);
-  
-          // Transfer content to HTML
-          var city1 = $(".weather").append("<div>" + response.name + ": Tempe (F) " + ((response.main.temp - 273.15) * 1.80 + 32).toFixed(2)  + "</div>");
-          city1.append(("<img id='wicon' src='' alt='Weather icon'>"));
-    
-          $('#wicon').attr('src', iconurl);
-  
-        })
-  
-    });
-  }
-  currentlocationWeather()
-
-
-  function cityWeather(){
-    var apiKey = "03e24d7d731fc83efc64f5aa4eb937c1";
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-
-    // Here we run our AJAX call to the OpenWeatherMap API
+  var apiKey = "03e24d7d731fc83efc64f5aa4eb937c1";
+  navigator.geolocation.getCurrentPosition(function (position) {
+    longitude = position.coords.longitude;
+    latitude = position.coords.latitude;
+    console.log(longitude)
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
     $.ajax({
       url: queryURL,
       method: "GET"
     })
-      // We store all of the retrieved data inside of an object called "response"
       .then(function (response) {
-  
+        console.log(response)
         var iconcode = response.weather[0].icon;
         var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-  
+
         // Log the queryURL
         console.log(queryURL);
-  
+
         // Log the resulting object
         console.log(response);
-        $(".weather").empty()
+
         // Transfer content to HTML
-        var city2 = $(".weather").html("<div>" + response.name + ": Tempe (F)" + ((response.main.temp - 273.15) * 1.80 + 32).toFixed(2)  + "</div>");
-          city2.append(("<img id='wicon' src='' alt='Weather icon'>"));
-    
-          $('#wicon').attr('src', iconurl);
-  
-      });
-  }
+        var city1 = $(".weather").append("<div>" + response.name + ": Tempe (F) " + ((response.main.temp - 273.15) * 1.80 + 32).toFixed(2) + "</div>");
+        city1.append(("<img id='wicon' src='' alt='Weather icon'>"));
 
-$(".btn").on("click", function() {
-        $(".search-area").removeClass();
-        // $(".search-area").addClass("");
-        var caContent = $("#get-city")
+        $('#wicon').attr('src', iconurl);
 
-        $(".event-data").empty()
-        var letsGo = caContent.val()
-        city = letsGo
-        pullingEvents()
+      })
 
-        cityWeather()
+  });
+}
+currentlocationWeather()
+
+
+function cityWeather() {
+  var apiKey = "03e24d7d731fc83efc64f5aa4eb937c1";
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+  // Here we run our AJAX call to the OpenWeatherMap API
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    // We store all of the retrieved data inside of an object called "response"
+    .then(function (response) {
+
+      var iconcode = response.weather[0].icon;
+      var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+
+      // Log the queryURL
+      console.log(queryURL);
+
+      // Log the resulting object
+      console.log(response);
+      $(".weather").empty()
+      // Transfer content to HTML
+      var city2 = $(".weather").html("<div>" + response.name + ": Tempe (F)" + ((response.main.temp - 273.15) * 1.80 + 32).toFixed(2) + "</div>");
+      city2.append(("<img id='wicon' src='' alt='Weather icon'>"));
+
+      $('#wicon').attr('src', iconurl);
+
+    });
+}
+
+$(".btn").on("click", function () {
+  $(".search-area").removeClass();
+  // $(".search-area").addClass("");
+  var caContent = $("#get-city")
+
+  $(".event-data").empty()
+  var letsGo = caContent.val()
+  city = letsGo
+  pullingEvents()
+
+  cityWeather()
 });
